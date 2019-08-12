9020992c41a1541f0e98d4579a63cab526a55022
Aleksandr Sasimov <asasimov@assist.ru>
2019-08-12 19:42:15 +0300

    Продолжение рефакторинга

--- src/test/java/ru/otus/service/ElementResolver.java
+++ src/test/java/ru/otus/service/ElementResolver.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Service;
+import ru.otus.page.BasePage;
 import ru.otus.page.NameOfElement;
 
 import java.lang.reflect.Field;
@@ -13,22 +14,22 @@ public class ElementResolver {
 
     private static final Logger logger = LogManager.getLogger(ElementResolver.class);
 
-    public WebElement getElementByName(String cucumberElementName, Object obj) {
-        Class<?> clazz = obj.getClass();
+    public WebElement getElementByName(String cucumberElementName, BasePage page) {
+        Class<?> clazz = page.getClass();
         for (Field field : clazz.getDeclaredFields()) {
             field.setAccessible(true);
             if (field.isAnnotationPresent(NameOfElement.class)) {
                 NameOfElement nameOfElementAnnotation = field.getAnnotation(NameOfElement.class);
                 if (nameOfElementAnnotation.value().equals(cucumberElementName)) {
                     try {
-                        return (WebElement) field.get(obj);
+                        return (WebElement) field.get(page);
                     } catch (IllegalAccessException ex) {
-                        logger.error("Is no such element with name {} at page {}", cucumberElementName, obj.getClass().getName());
+                        logger.error("Is no such element with name {} at page {}", cucumberElementName, page.getClass().getName());
                     }
                 }
             }
         }
-        throw new IllegalArgumentException("ERROR: there is no such element with name " + cucumberElementName + " at page " + obj.getClass().getName());
+        throw new IllegalArgumentException("ERROR: there is no such element with name " + cucumberElementName + " at page " + page.getClass().getName());
     }
 
 }
